621a988d91e6185725f1e3a9d21e72f79c3d1b62
Anthony Oglesby <ogle1ant@gmail.com>
2020-03-12 17:41:00 -0400

    adding comments to index.js

--- garden-tracker-frontend/src/index.js
+++ garden-tracker-frontend/src/index.js
@@ -1,9 +1,12 @@
+// initial fetch for plants
 Api.fetchPlants()
 
+// setting up some variables
 const parseJSON = response => response.json()
 const main = document.getElementById('main')
 const menu = document.getElementById('menu')
 
+// renders Garden Cards on page load.
 document.addEventListener("DOMContentLoaded", () => {
     Api.fetchGardens()
     .then(resObj => {
@@ -16,8 +19,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }) 
 })
 
+// adds click listeners to all spans in div.menu
 menu.addEventListener('click', handleMenuClick)
 
+// handles menu clicks + clears page to declutter
 function handleMenuClick(event){
     if (event.target.id !== menu){
     	clearWindow()
@@ -26,19 +31,17 @@ function handleMenuClick(event){
     }
 }
 
+// connects html span id to functions associated with handleMenuClick
 const callbacks= {
     allPlants: renderAllPlants
 }
 
+//calls the render function from Plants
 function renderAllPlants(){
 	Plant.renderPlants()
 }
 
+// clears window to declutter
 function clearWindow(){
 	document.getElementById("main").innerHTML = ""
-}
-
-
-function clearNewProject() {
-    document.querySelector("#select").innerHTML = ""  
 }
