1fc28582d3e967de391cbe22d21d0b9f48c16cd2
Anthony Oglesby <ogle1ant@gmail.com>
2020-03-12 14:04:25 -0400

    initial build out of js classes

---
+++ garden-tracker-frontend/src/garden.js
@@ -0,0 +1,8 @@
+class Garden{
+	constructor(id, title, gardenType){
+		this.id = id
+		this.title = title
+		this.gardenType = gardenType
+	}
+
+}
---
+++ garden-tracker-frontend/src/plant.js
@@ -0,0 +1,9 @@
+class Plant{
+	constructor(id, plantName, plantType, plantFamily, garden_id){
+		this.id = id
+		this.plantName = plantName
+		this.plantType = plantType
+		this.plantFamily = plantFamily
+		this.garden_id = garden_id
+	}
+}
