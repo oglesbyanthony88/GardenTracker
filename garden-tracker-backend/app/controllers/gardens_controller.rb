ec031f88f5220d24e0a352529f301e02dca4130e
Anthony Oglesby <ogle1ant@gmail.com>
2020-03-12 13:41:09 -0400

    building out controllers

--- garden-tracker-backend/app/controllers/gardens_controller.rb
+++ garden-tracker-backend/app/controllers/gardens_controller.rb
@@ -1,12 +1,21 @@
 class GardensController < ApplicationController
 
 	def index
+		gardens = Gardens.all
+
+		render json: GardenSerializer.new(gardens)
+	end
+
+	def show
+		garden = Garden.find_by(params[:id])
+
+		render json: GardenSerializer.new(garden)
 	end
 
 private
 
 	def garden_params
-
+		params.require(:garden).permit(:title, :gardenType)
 	end
-	
+
 end
--- garden-tracker-backend/app/controllers/plants_controller.rb
+++ garden-tracker-backend/app/controllers/plants_controller.rb
@@ -1,12 +1,15 @@
 class PlantsController < ApplicationController
 
 	def index
+		plants = Plants.all
+
+		render json: PlantsSerializer.new(plants)
 	end
 
 private
 
 	def plant_params
-
+		params.require(:plant).permit(:plantName, :plantType, :plantFamily, :garden_id)
 	end
 
 
